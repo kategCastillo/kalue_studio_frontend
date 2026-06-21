24eb3e3205f074ea72ae062d3fc5e65504f9bc78
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-06-21 18:39:30 -0500

    se crea Frontend (coleccion, productos, nosotros y contacto)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Coleccion } from './pages/coleccion/coleccion';
+import { Productos } from './pages/productos/productos';
+import { Nosotros } from './pages/nosotros/nosotros';
+import { Contacto } from './pages/contacto/contacto';
+
+export const routes: Routes = [
+  {
+    path: 'coleccion',
+    component: Coleccion
+  },
+  {
+    path: 'productos',
+    component: Productos
+  },
+  {
+    path: 'nosotros',
+    component: Nosotros
+  },
+  {
+    path: 'contacto',
+    component: Contacto
+  }
+];
---
+++ src/app/pages/coleccion/coleccion.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coleccion',
+  imports: [],
+  templateUrl: './coleccion.html',
+  styleUrl: './coleccion.css',
+})
+export class Coleccion {}
