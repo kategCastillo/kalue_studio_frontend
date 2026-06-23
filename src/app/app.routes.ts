91faec3c71b913f2764a58cc691ca4ef5fad1346
szaratep <sebaszarate2005@gmail.com>
2026-06-23 14:22:59 -0500

    se crea el registro completo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: 'carrito', loadComponent: () => import('./features/carrito/carrito') },
-    { path: 'login', loadComponent: () => import('./features/login/login') }
+    { path: 'login', loadComponent: () => import('./features/login/login') },
+    { path: 'registro', loadComponent: () => import('./features/register/register') }
 
 ]
