6c32d55ad790aad4bb2b69881ddfe03d27ca0861
szaratep <sebaszarate2005@gmail.com>
2026-07-06 12:55:50 -0500

    [+]: Se adelanta el modulo de registro de usuario, y de registro de usuario de administrador

--- src/app/features/register/register.ts
+++ src/app/features/register/register.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export default class Register {}
+export default class Register {
+  public formData: FormGroup;
+
+  constructor (){
+    this.formData = new FormGroup ({
+      name: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+      nickname: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+      email: new FormControl ('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      label: new FormControl ('', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]),
+      receiverName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+      address: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      phone: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSend(){
+    if(this.formData.valid){
+      console.log(this.formData.value);
+    }else{
+      console.log('formulario invalido');
+    }
+  }
+
+  registerWithGoogle(){
+
+  }
+
+  registerWithapple(){
+    
+  }
+
+}
--- src/app/features/users/user-new-form/user-new-form.ts
+++ src/app/features/users/user-new-form/user-new-form.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-new-form',
@@ -8,23 +8,28 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './user-new-form.css',
 })
 export default class UserNewForm {
-  formData: FormGroup;
+  public formData: FormGroup;
 
   constructor(){
     this.formData = new FormGroup({
-      name: new FormControl(),
-      nickname: new FormControl(),
-      email: new FormControl(),
-      password: new FormControl(),
-      role: new FormControl(),
-      status: new FormControl(),
-      avatar: new FormControl()
-
+      name: new FormControl( '', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+      nickname: new FormControl( '', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+      email: new FormControl( '', [ Validators.required, Validators.email ] ),
+      password: new FormControl( '', [Validators.required]),
+      role: new FormControl( '', [Validators.required]),
+      status: new FormControl( true ),
+      avatar: new FormControl( '' )
     });
   }
 
   onSend() {
-    //Muestro los valores que capturo el formulario
-    console.log(this.formData.value);
+    //verifica si el campo es valido
+    if(this.formData.valid) {
+      //Muestro los valores que capturo el formulario
+      console.log(this.formData.value);
+    }else{
+      console.log('formulario invalido');
+    }
+
   }
 }
