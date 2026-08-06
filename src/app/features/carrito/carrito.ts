8d0a4f93222412c19886be52e6338545483c581f
szaratep <sebaszarate2005@gmail.com>
2026-08-05 21:34:28 -0500

    [+] se conecta el carrito con los productos y demas

--- src/app/features/carrito/carrito.ts
+++ src/app/features/carrito/carrito.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardItemsCarrito } from "../../shared/components/card-items-carrito/card-items-carrito";
+import { HttpProducts } from '../../core/services/http-products';
+import { BehaviorSubject } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTruck, faRotateLeft, faCreditCard, faShield, faShieldCat, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-carrito',
-  imports: [CardItemsCarrito],
+  imports: [CardItemsCarrito, AsyncPipe, FontAwesomeModule, RouterLink],
   templateUrl: './carrito.html',
   styleUrl: './carrito.css',
 })
-export default class Carrito {}
+export default class Carrito {
+  private httpProducts = inject(HttpProducts);
+  public listProduct$ = new BehaviorSubject<any>([]);
+
+  //FONTAWESOME
+  public faTruck = faTruck;
+  public faRotateLeft = faRotateLeft;
+  public faCreditCard = faCreditCard;
+  public faShield = faShield;
+  public faShieldCat = faShieldCat;
+  public faArrowRigth = faArrowRight;
+
+  private loadProducts () {
+    this.httpProducts.getProduct().subscribe({
+      next: (data) => {
+        this.listProduct$.next(data.data);
+      },
+      error: (error) => {
+        console.error(error)
+      },
+      complete: () => {}
+    })
+  }
+
+  ngOnInit (){
+    this.loadProducts();
+  }
+
+}
--- src/app/shared/components/card-items-carrito/card-items-carrito.ts
+++ src/app/shared/components/card-items-carrito/card-items-carrito.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus, faTrash, faMinus } from '@fortawesome/free-solid-svg-icons';
+
+
 
 @Component({
   selector: 'app-card-items-carrito',
-  imports: [],
+  imports: [CurrencyPipe, FontAwesomeModule],
   templateUrl: './card-items-carrito.html',
   styleUrl: './card-items-carrito.css',
 })
-export class CardItemsCarrito {}
+export class CardItemsCarrito {
+  @Input() product:any;
+  public count: number = 0;
+  
+  //FONTAWESOME
+  public faPlus = faPlus;
+  public faTrash = faTrash;
+  public faMinus = faMinus;
+
+  increment(){
+    this.count++;
+  }
+
+  decrement(){
+    this.count--;
+  }
+}
