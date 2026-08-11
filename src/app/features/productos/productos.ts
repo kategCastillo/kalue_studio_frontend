1b4acacf7b3bd0b1d8fe47f341d0ba621a5835b0
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-08-10 21:54:52 -0500

    se crean los diseños

--- src/app/features/productos/productos.ts
+++ src/app/features/productos/productos.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpProducts } from '../../core/services/http-products';
+import { BehaviorSubject } from 'rxjs';
+import { ProductCard } from "../../shared/components/product-card/product-card";
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-productos',
-  imports: [],
+  imports: [ProductCard, AsyncPipe],
   templateUrl: './productos.html',
   styleUrl: './productos.css',
 })
-export default class Productos {}
+export default class Productos {
+  private httpProducts = inject (HttpProducts)
+  public listProducts$ = new BehaviorSubject<any>([]) 
+
+  private loadProduct () {
+    this.httpProducts.getProduct().subscribe({
+      next: (data) => {
+        this.listProducts$.next(data.data)
+      },
+
+      error: (error) => {
+        console.error(error)
+      },
+
+      complete: () => {}
+    })
+  }
+
+  ngOnInit () {
+    this.loadProduct()
+  }
+}
