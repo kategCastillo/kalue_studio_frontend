c2485af3f212f9363ece490a6e6e662b8e8b2fae
szaratep <sebaszarate2005@gmail.com>
2026-08-13 18:09:53 -0500

    [+]Se eliminan las variantes y se actualiza el header

--- src/app/features/variants/variants-new-form/variants-new-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-variants-new-form',
-  imports: [],
-  templateUrl: './variants-new-form.html',
-  styleUrl: './variants-new-form.css',
-})
-export default class VariantsNewForm {}
